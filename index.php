6fd48d21495ade114caebbd796ec81a457e9c342
juan manuel <elmaccapabayona@gmail.com>
2016-03-02 00:08:02 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1> sueña nunca dejes de soñar... si pones tu alma y tu corazon en un sueño, el universo conspira para ayudarte a conseguirlo</h1>
+</body>
+</html>
