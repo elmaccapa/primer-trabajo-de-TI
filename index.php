f9b195f2cdd5744e44a704f06e2b78b86e618574
juan manuel <elmaccapabayona@gmail.com>
2016-03-02 00:30:30 -0500

    iniciando

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<h1> sueña nunca dejes de soñar... si pones tu alma y tu corazon en un sueño, el universo conspira para ayudarte a conseguirlo</h1>
+	<h1> sueña nunca dejes de soñar... si pones tu alma y tu corazon en un sueño, el universo conspira para ayudarte a conseguirlo.</h1>
+	<h2>  “El hombre más poderoso es el que es dueño de sí mismo.”</h2>
 </body>
 </html>
